4dd9ae5308194c36d1929fb09fad625844e9cd20
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-01-23 11:28:18 -0500

    Add line graph by scripting in pip

--- when_should_you_resign_pip.R
+++ when_should_you_resign_pip.R
@@ -24,4 +24,13 @@ plot(t)
 ggplot(.df, aes(x=Category, fill=Result))+geom_bar(position = "fill")
 
 # line graph % winning
+ggplot(df, aes(x=worst_white_eval_bucket)) + geom_line(stat="summary", fun.y="")
 
+t <- aggregate(
+  df$Result,
+  list(Worst_Eval=df$worst_white_eval_bucket, Time_Control=df$Category),
+  function(x) (.t <- table(x))/sum(.t)
+)
+t$White_Wins <- t$x[,"1-0"]
+ggplot(t, aes(x=Worst_Eval, y=White_Wins, group=1)) + geom_line() + ylim(0,1)
+ggplot(t, aes(x=Worst_Eval, y=White_Wins, group=Time_Control)) + geom_line() + ylim(0,1)
