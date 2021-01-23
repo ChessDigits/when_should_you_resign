a5157bc9ba46f338b40d47c7f511cf1f48b3d5a3
Patrick Coulombe <patrick.coulombe@gmail.com>
2021-01-23 10:12:45 -0500

    Make mate evaluations as global variables and change from 999 to 200

--- when_should_you_resign_pip.R
+++ when_should_you_resign_pip.R
@@ -9,3 +9,12 @@ pip
 df <- load_data()
 df <- replace_mates_with_extreme_evaluations(df)
 df <- add_worst_eval_for_each_player(df)
+
+
+
+hist(df$worst_black_eval)
+hist(df$worst_white_eval)
+with(subset(df, worst_black_eval > 5), table(Result))
+with(subset(df, worst_black_eval == WHITE_MATE_EVAL), table(Result))
+t <- with(subset(df, worst_black_eval >= WHITE_MATE_EVAL), xtabs(~Category+Result))
+plot(t)
